811446192b088520cb58471e5390bb4c8950e1ee
paoladiaz <paoladiaz@localhost>
2023-10-25 15:50:00 -0500

    Got to gcd
    Signed-off-by: paoladiaz <paoladiaz@localhost>

--- src/studio6/RecursiveMethods.java
+++ src/studio6/RecursiveMethods.java
@@ -12,9 +12,12 @@ public class RecursiveMethods {
 	 *         ...)
 	 */
 	public static double geometricSum(int n) {
-		
-			// FIXME compute the geometric sum for the first n terms recursively
-			return 0;
+			if(n == 0)
+				return 0;
+			else 
+				return Math.pow(0.5, n)+ geometricSum(n-1) ;
+			
+			
 		
 	}
 
@@ -27,9 +30,12 @@ public class RecursiveMethods {
 	 * @return greatest common divisor of p and q
 	 */
 	public static int gcd(int p, int q) {
-		
+		if( q == 0)
+			return p;
+		else
+			return gcd(q , p%q);
 			// FIXME compute the gcd of p and q using recursion
-			return 0;
+			
 		
 	}
 
